f2c6e5aca44e8b99649802599b7a86cc2ea7fa2a
Daniel Ramirez <77579481+drqa@users.noreply.github.com>
2021-12-10 10:53:02 +0300

    Create day10.py

---
+++ day10.py
@@ -0,0 +1,51 @@
+def toString(ls):
+    pairs = {"(": ")", "[": "]", "{": "}", "<": ">"}
+    s = ''.join(ls[::-1])
+    complete = ""
+    for c in s:
+        complete += pairs[c]
+    return complete
+
+def valid(line):
+    left_sides = []
+    for i in range(len(line)):
+        if line[i] in ["(", "{", "[", "<"]:
+            left_sides.append(line[i])
+        elif line[i] == ')' and len(left_sides) != 0 and left_sides[-1] == '(':
+            left_sides.pop()
+        elif line[i] == '}' and len(left_sides) != 0 and left_sides[-1] == '{':
+            left_sides.pop()
+        elif line[i] == ']' and len(left_sides) != 0 and left_sides[-1] == '[':
+            left_sides.pop()
+        elif line[i] == '>' and len(left_sides) != 0 and left_sides[-1] == '<':
+            left_sides.pop()
+        else:
+            return False
+    return left_sides 
+
+with open("input") as file:
+    lines = [x.strip() for x in file]
+
+# Part 1
+corrupted_scores = {")": 3, "]": 57, "}": 1197, ">": 25137}
+score = 0
+for line in lines:
+    res = valid(line)
+    if res != True:
+        score += corrupted_scores[res]
+print(score)
+
+# Part 2
+scores_dict = {")": 1, "]": 2, "}": 3, ">": 4}
+scores = []
+for line in lines:
+    score = 0
+    res = valid(line)
+    if res != False:
+        s = toString(res)
+        for c in s:
+            score *= 5
+            score += scores_dict[c]
+        scores.append(score)
+scores.sort()
+print(scores[len(scores) // 2])
